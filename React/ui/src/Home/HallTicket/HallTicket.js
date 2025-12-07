19b85963f80143d953aef082670995e0ea5136ca
Lokesh0018 <munakalalokesh222@gmail.com>
2025-12-07 11:09:46 +0530

    HallTicket Creation

--- React/ui/src/Home/HallTicket/HallTicket.js
+++ React/ui/src/Home/HallTicket/HallTicket.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { IoMdDownload } from "react-icons/io";
 
 const HallTicket = () => {
+    const Year = 4;
+    const Month = "October";
+    const YearExam = 2025;
+    const Semester = 1;
     return (
         <div className="hallTicket">
             <div className="pageHeader ht">
@@ -8,20 +13,140 @@ const HallTicket = () => {
             </div>
             <div className="hallTicketContainer">
                 <div className="ht">
-                    <div className="htHeader">
+                    <div className="htHeader1">
                         <img
                             src="/img/clgLogo.jpg"
                             alt="College Logo"
                             className="clgLogo"
                         />
                         <div className="htTitle">
-                        <h1 className="headerTxt1">RAGHU INSTITUTE OF TECHNOLOGY</h1>
-                        <h2 className="headerTxt2">(Autonomous)</h2>
-                        <p>(Approved by AICTE, New Delhi & Permanently Affiliated to JNTUK, JNTUK, Kakinada)</p>
+                            <h1 className="insName">RAGHU INSTITUTE OF TECHNOLOGY</h1>
+                            <h2 className="subHeader">(Autonomous)</h2>
+                            <p className="accreditationText">(Approved by AICTE, New Delhi & Permanently Affiliated to JNTUGV, Gurajada)</p>
+                            <p className="accreditationText">Accredited by NBA (ECE, EEE, MECH, CSE) & NAAC with 'A' Grade, Listed u/s 2(f) & 12(8) of UGC Act 1956</p>
+                            <p className="accreditationText"> Dakamarri, Bheemunipamam Mandal, Visakhapatnam Dist. 531 162 (Α.Ρ.)</p>
+                            <p className="accreditationText">Ph: +91-8922-248003, 248013 E-mail: principal@raghuinstech.com website: www.raghuinstech.com</p>
+                            <b className="examTitle">B TECH {Year} YEAR {Semester} SEMESTER (AR20) Regular Examinations , {Month} {YearExam} </b>
                         </div>
+                    </div>
+                    <div className="branchLable">
+                        <b>CSE</b>
+                        <b><u>HALL TICKET</u></b>
+                        <b>ORIGINAL</b>
+                    </div>
+                    <div className="htLable">
+                        <table>
+                            <tr>
+                                <td>Hall Ticket No: </td>
+                                <td>233J5A0513</td>
+                            </tr>
+                            <tr>
+                                <td>Student Name: </td>
+                                <td>MUNAKALA LOKESH</td>
+                            </tr>
+                            <tr>
+                                <td>Father Name: </td>
+                                <td>MUNAKALA SRINIVASA RAO</td>
+                            </tr>
+                        </table>
+                        <img src="/img/Img2.jpeg" className="htImg" />
+                    </div>
+                    <div class="htInfo">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>S.No</th>
+                                    <th>Sub Code</th>
+                                    <th>Subject Name</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Sign of the Invigilator</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>20ES3019</td>
+                                    <td>Discrete Mathematical Structures</td>
+                                    <td>02.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>2</td>
+                                    <td>20CS2002</td>
+                                    <td>Python Programming</td>
+                                    <td>04.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>3</td>
+                                    <td>20CS3002</td>
+                                    <td>Computer Organization and Architecture</td>
+                                    <td>06.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>4</td>
+                                    <td>20CS3004</td>
+                                    <td>Object Oriented Programming through C++</td>
+                                    <td>08.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>5</td>
+                                    <td>20HS3002</td>
+                                    <td>Managerial Economics and Financial Analysis</td>
+                                    <td>10.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>6</td>
+                                    <td>20HS3002</td>
+                                    <td>Managerial Economics and Financial Analysis</td>
+                                    <td>10.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>7</td>
+                                    <td>20HS3002</td>
+                                    <td>Managerial Economics and Financial Analysis</td>
+                                    <td>10.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>8</td>
+                                    <td>20HS3002</td>
+                                    <td>Managerial Economics and Financial Analysis</td>
+                                    <td>10.01.2024</td>
+                                    <td>10:00 AM To 01:00 PM</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="htFooter">
+                        <b>Signature of the Student</b>
+                        <b>Controller of Examinations</b>
+                        <b>Principal</b>
                     </div>
                 </div>
             </div>
+            <button className="addBtn htdBtn"><IoMdDownload />Download</button>
         </div>
     )
 }

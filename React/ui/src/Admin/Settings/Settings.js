8526bcf257980b1672681de2ca5d2f8e7697c0fa
Lokesh0018 <munakalalokesh222@gmail.com>
2025-12-08 17:47:00 +0530

    Updated Settings

--- React/ui/src/Admin/Settings/Settings.js
+++ React/ui/src/Admin/Settings/Settings.js
@@ -1,29 +1,92 @@
-import React from "react";
-import { MdLogout } from 'react-icons/md';
-
+import React, { useState } from "react";
+import { MdLogout,MdOutlineSave  } from "react-icons/md";
 const Settings = () => {
+    const [activeTab, setActiveTab] = useState(false);
+
     return (
         <div className="settings">
-            <div className="pageHeader">
-                <div className="pageTitle">Profile Settings</div>
-            </div>
-            <div className="settingsContainer">
-                    <h3>Admin Name:</h3>
-                <div className="detailsContainer">
-                    <span className="value">John Doe</span>
+
+            <ul className="settingsList">
+                <li
+                    className={activeTab === false ? "active" : ""}
+                    onClick={() => setActiveTab(false)}
+                >
+                    General
+                </li>
+
+                <li
+                    className={activeTab === true ? "active" : ""}
+                    onClick={() => setActiveTab(true)}
+                >
+                    Profile
+                </li>
+            </ul>
+
+            {(activeTab) ? (
+                <div className="profileSettings">
+                    <div className="pageHeader">
+                        <div className="pageTitle">
+                            Profile Settings
+                        </div>
+                    </div>
+                    <div className="settingsContainer">
+                        <h3>Admin Name:</h3>
+                        <div className="detailsContainer">
+                            <span className="value">John Doe</span>
+                        </div>
+
+                        <h3 className="label">Admin Email:</h3>
+                        <div className="detailsContainer">
+                            <span className="value">admin@example.com</span>
+                        </div>
+
+                        <h3 className="label">College Name:</h3>
+                        <div className="detailsContainer">
+                            <span className="value">ABC Engineering College</span>
+                        </div>
+
+                        <button className="logoutBtn">
+                            Logout <MdLogout />
+                        </button>
+                    </div>
                 </div>
-                    <h3 className="label">Admin Email:</h3>
-                <div className="detailsContainer">
-                    <span className="value"></span>
+            ) : (
+                <div className="generalSettings">
+                    <div className="pageHeader">
+                        <div className="pageTitle">
+                            General Settings
+                        </div>
+                    </div>
+                    <div className="settingsContainer">
+                        <h3>Batch:</h3>
+                        <div className="detailsContainer">
+                            <span className="value">2023-2026</span>
+                        </div>
+
+                        <h3 className="label">Branch:</h3>
+                        <div className="detailsContainer">
+                            <input className="value" type="text" />
+                        </div>
+
+                        <h3 className="label">Year:</h3>
+                        <div className="detailsContainer">
+                            <input className="value" type="number" />
+                        </div>
+
+                        <h3 className="label">Semester:</h3>
+                        <div className="detailsContainer">
+                            <input className="value" type="number" />
+                        </div>
+
+                        <button className="logoutBtn">
+                            save <MdOutlineSave/>
+                        </button>
+                    </div>
                 </div>
-                    <h3 className="label">Collage Name:</h3>
-                <div className="detailsContainer">
-                    <span className="value"></span>
-                </div>
-            </div>
-                <button className="logoutBtn">Logout<MdLogout /></button>
+            )}
+
         </div>
     );
-}
+};
 
 export default Settings;

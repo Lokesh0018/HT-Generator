6b0afe8c5a3b14f1fc502875254fc734f1e0ce86
Lokesh0018 <munakalalokesh222@gmail.com>
2026-03-22 21:44:25 +0530

    Added auto TimeTable sending functionality

--- SpringBoot/API/htg/src/main/java/com/api/htg/HtgApplication.java
+++ SpringBoot/API/htg/src/main/java/com/api/htg/HtgApplication.java
@@ -2,8 +2,10 @@ package com.api.htg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HtgApplication {
 
 	public static void main(String[] args) {
